3a5abba37ece169071072b894924a324a77cc559
Ben Anding <benanding@Bens-MacBook-Air.local>
2019-01-30 15:03:17 -0600

    improved runall.sh and added cleanandlabel.py

--- runall.sh
+++ runall.sh
@@ -1,9 +1,10 @@
-python /Users/benanding/repos/NLP/webscrape/anthmagverse.py 
-python /Users/benanding/repos/NLP/webscrape/anthmasspoets.py 
-python /Users/benanding/repos/NLP/webscrape/elizabethanverse.py 
-python /Users/benanding/repos/NLP/webscrape/georgianverse.py 
-python /Users/benanding/repos/NLP/webscrape/newpoetry.py 
-python /Users/benanding/repos/NLP/webscrape/oxfordengverse.py 
-python /Users/benanding/repos/NLP/webscrape/oxfordvictverse.py 
-python /Users/benanding/repos/NLP/webscrape/restorationverse.py 
+mkdir ./authors
+python ./anthmagverse.py 
+python ./anthmasspoets.py 
+python ./elizabethanverse.py 
+python ./georgianverse.py 
+python ./newpoetry.py 
+python ./oxfordengverse.py 
+python ./oxfordvictverse.py 
+python ./restorationverse.py 
 
